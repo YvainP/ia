db0e8f94577e4c69b69228bd71d3a9fc8e02cc03
YvainP <47723677+YvainP@users.noreply.github.com>
2020-06-02 18:09:25 +0200

    Update utilisation.py

--- mymodel/utilisation.py
+++ mymodel/utilisation.py
@@ -5,16 +5,21 @@ import numpy
 import torchvision.transforms as transforms
 from PIL import Image
 
+#On transforme notre image en 32x32
 transform = transforms.Compose([            
     transforms.Resize(32),                   
     transforms.CenterCrop(32),              
     transforms.ToTensor(),                 
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+#On récupère l'image
 img = Image.open("cat.jpg")
+
+#On transforme cette image en tensor
 transformedImg = transform(img)
 resizedImg = torch.unsqueeze(transformedImg, 0)
 
+#classes connus par notre cnn
 classes = ('plane', 'car', 'bird', 'cat',
                    'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
@@ -39,11 +44,15 @@ class Net(nn.Module):
         return x
 
 net = Net()
+
 #Recharge le modèle
 PATH = 'model.pth'
 net.load_state_dict(torch.load(PATH))
+
+#notre cnn va étudier l'image
 outputs = net(resizedImg)
 
+#et affiche sa prédiction
 _, predicted = torch.max(outputs, 1)
 print("Prédiction sur l'image".join('%5s' % classes[predicted[j]] for j in range(1)))
 
