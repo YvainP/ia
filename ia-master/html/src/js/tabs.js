16cfe634fdf16dc8384452e8fb58e06d2c1aa0f8
YvainP <47723677+YvainP@users.noreply.github.com>
2020-06-02 18:39:20 +0200

    update tuto

---
+++ ia-master/html/src/js/tabs.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var $tabs = $('#tabs').tabs();
+
+    //get selected tab
+    function getSelectedTabIndex() {
+        return $tabs.tabs('option', 'active');
+    }
+
+    //get tab contents
+    beginTab = $("#tabs ul li:eq(" + getSelectedTabIndex() + ")").find("a");
+
+    loadTabFrame($(beginTab).attr("href"),$(beginTab).attr("rel"));
+
+    $("a.tabref").click(function() {
+        loadTabFrame($(this).attr("href"),$(this).attr("rel"));
+    });
+
+    //tab switching function
+    function loadTabFrame(tab, url) {
+        if ($(tab).find("iframe").length == 0) {
+            var html = [];
+            html.push('<div class="openout"><a href="' + url + '"></a></div><iframe class="iframetab" src="' + url + '">Load Failed?</iframe>');
+            html.push('</div>');
+            $(tab).append(html.join(""));
+            $(tab).find("iframe").height($(window).height()-70);
+        }
+        return false;
+    }
+});
